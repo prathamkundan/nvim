9d051f9c4e05eb65fc7faf3704929eef7c1f767c
Pratham Kundan <prathamkundan@gmail.com>
2024-03-18 17:40:57 +0530

    Added binding to include non git files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>f', function() builtin.find_files({ no_ignore = true, }) end, {})
 vim.keymap.set('n', '<leader>fo', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
